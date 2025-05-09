d53c5d307d65612749f0a0097b38dec8316903bb
destadrns <dstaasptra@gmail.com>
2025-05-09 17:07:27 +0700

    praktikum 6

---
+++ vue-project/src/views/Experience.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container my-5" data-aos="fade-left">
+    <h2 class="h3 mb-4 text-primary">Pengalaman</h2>
+    <div class="row g-4">
+      <!-- Card 1 -->
+      <div class="col-md-9">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text">
+              <strong>Gelar:</strong> S.IKom Informatika<br>
+              <strong>Tahun:</strong> 2023 – Now
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
